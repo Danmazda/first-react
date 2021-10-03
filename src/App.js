d17acbd50ff8645c5a1b409e37032d538e238d26
Danilo Procopio <dan_mazda@hotmail.com>
2021-10-03 19:17:13 -0300

    aula 15

--- src/App.js
+++ src/App.js
@@ -1,52 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 // Cria uma classe App que importa da super classe Component
 class App extends Component{
-  //constructor sempre usa props
-  constructor(props){
-    //Usa a super classe para criar o objeto
-    super(props);
-    // Alteração do estado do objeto
-    this.state = { 
-      name: 'Danilo Procópio',
-      counter: 0
+    // state pode ser usado direto sem o construtor (this.state)
+    state = { 
+     posts: [
+       {
+         id: 1,
+         title: 'Título 1',
+         body: 'Corpo 1'
+       },
+       {
+        id: 2,
+        title: 'Título 2',
+        body: 'Corpo 2'
+      },
+      {
+        id: 3,
+        title: 'Título 3',
+        body: 'Corpo 3'
+      }
+     ]
     };
-  }
-  // Usa-se arrow function, pois ela n tem this, assim quando se usa this esta se referindo a própria classe
-  handlePClick = () =>{
-    // A cada alteração de estado render é chamado novamente 
-    this.setState({name: 'Gilberto'});
-  }
 
-  handleAClick = (event)=>{
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({counter: counter+1});
-  }
-
-  render(){
+  render() {
     // Cria variável de mesmo nome da chave do objeto
-    const { name, counter } = this.state;
-    return(<div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p onClick={this.handlePClick}>
-                {name} {counter}
-              </p>
-              <a onClick={this.handleAClick}
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header> 
-          </div>);
-  }
-}
+    const { posts } = this.state;
+    // map faz a função para cada elemento da lista sendo esse elemento post
+    return(
+      //posts.map deve retornar somente uma tag, denro dessa tag podem ter quantas quiser
+    <div className="App">
+      {posts.map(post =>(
+        <div className='myDiv' key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>);
+  };}
+
 // function App() {
 //   return (
 //     <div className="App">
