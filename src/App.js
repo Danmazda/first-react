00afc6b8eeee9d48489396f0f35a1df13a125b5a
Danilo Procopio <dan_mazda@hotmail.com>
2021-10-03 18:57:21 -0300

    aula 14

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Cria uma classe App que importa da super classe Component
+class App extends Component{
+  //constructor sempre usa props
+  constructor(props){
+    //Usa a super classe para criar o objeto
+    super(props);
+    // Alteração do estado do objeto
+    this.state = { 
+      name: 'Danilo Procópio',
+      counter: 0
+    };
+  }
+  // Usa-se arrow function, pois ela n tem this, assim quando se usa this esta se referindo a própria classe
+  handlePClick = () =>{
+    // A cada alteração de estado render é chamado novamente 
+    this.setState({name: 'Gilberto'});
+  }
+
+  handleAClick = (event)=>{
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({counter: counter+1});
+  }
+
+  render(){
+    // Cria variável de mesmo nome da chave do objeto
+    const { name, counter } = this.state;
+    return(<div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p onClick={this.handlePClick}>
+                {name} {counter}
+              </p>
+              <a onClick={this.handleAClick}
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header> 
+          </div>);
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Olá mundo
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
