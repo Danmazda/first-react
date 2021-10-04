d949d6905ce3f5c50ed388392c234d867b9fad1a
Danilo Procopio <dan_mazda@hotmail.com>
2021-10-04 20:01:09 -0300

    aula 16

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Component } from 'react';
 // Cria uma classe App que importa da super classe Component
 class App extends Component{
     // state pode ser usado direto sem o construtor (this.state)
-    state = { 
+    state = {
+      counter: 0,
      posts: [
        {
          id: 1,
@@ -23,14 +24,38 @@ class App extends Component{
       }
      ]
     };
+  //Para ter acesso ao timeout de qlqr parte dentro da classe
+  timeoutUpdate = null;
+  handleTimeOut = () => {
+    const {posts, counter} = this.state;
+    posts[0].title = 'O título mudou';
+    if(counter <= 5){
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({posts: posts, counter: counter+1});
+    }, 1000);}
+    else{
+      console.log('Counting is done')
+    }
+  }
 
+  componentDidMount(){
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate(){
+    this.handleTimeOut();
+  };
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate);
+  };
   render() {
     // Cria variável de mesmo nome da chave do objeto
-    const { posts } = this.state;
+    const { posts, counter} = this.state;
     // map faz a função para cada elemento da lista sendo esse elemento post
     return(
       //posts.map deve retornar somente uma tag, denro dessa tag podem ter quantas quiser
     <div className="App">
+      <h2>{counter}</h2>
       {posts.map(post =>(
         <div className='myDiv' key={post.id}>
           <h1>{post.title}</h1>
